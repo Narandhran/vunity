c4e814952043a2e39cfa8e4c9971b8b2473c28a6
narandhran <narandhran@gmail.com>
2020-08-18 09:48:35 +0530

    db auth added

--- db.js
+++ db.js
@@ -4,7 +4,8 @@ var database = {
     connectivity: (config) => {
         mongoose.set('debug', true);
         mongoose.Promise = global.Promise;
-        mongoose.connect(`${config.DB_HOST}:${config.DB_PORT}/${config.DB_NAME}`, { useCreateIndex: true, useFindAndModify: false, useUnifiedTopology: true, useNewUrlParser: true });
+        mongoose.connect(`mongodb://${config.DB_USER}:${config.DB_PASS}@${config.DB_HOST}:${config.DB_PORT}/${config.DB_NAME}`,
+            { useCreateIndex: true, useFindAndModify: false, useUnifiedTopology: true, useNewUrlParser: true });
         const db = mongoose.connection;
         return db;
     }
