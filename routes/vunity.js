a417da6fe8ac4aaa92f2a0604231d5ea24d6131c
narandhran <narandhran@gmail.com>
2020-08-23 15:46:05 +0530

    filter method to POST

--- routes/vunity.js
+++ routes/vunity.js
@@ -7,6 +7,6 @@ module.exports = app => {
     app.get('/vunity/get_by_user/:id', AllUsers, vunityCtl.findByUserId);
     app.put('/vunity/update_photo', AllUsers, vunityCtl.updatePhoto);
     app.get('/vunity/list', AllUsers, vunityCtl.list);
-    app.get('/vunity/filter', AllUsers, vunityCtl.customFilter);
+    app.post('/vunity/filter', AllUsers, vunityCtl.customFilter);
     app.get('/vunity/search/:search', AllUsers, vunityCtl.customSearch);
 };
