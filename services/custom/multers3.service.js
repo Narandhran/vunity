5a7f5ea4b586aa153c0fd5a591ecc22c75a1b31b
narandhran <narandhran@gmail.com>
2020-10-19 10:08:23 +0530

    video-thumb field name included

--- services/custom/multers3.service.js
+++ services/custom/multers3.service.js
@@ -30,7 +30,7 @@ var loadMulter = (fileSize, filePath) => {
                     cb(null, true);
                 } else cb('Unsupported file type', false);
             } else if (file.fieldname == 'category' || file.fieldname == 'dp'
-                || file.fieldname == 'pdf-thumb' || file.fieldname == 'banner') {
+                || file.fieldname == 'pdf-thumb' || file.fieldname == 'banner' || file.fieldname == 'video-thumb') {
                 let fext = path.extname(file.originalname);
                 if (fext == '.jpg' || fext == '.jpeg' || fext == '.png') {
                     cb(null, true);
